0fab7398cb5440e30df43afac3321d153245301e
Micael Ravaglia <micael.ravaglia@gmail.com>
2015-11-11 16:35:47 +0100

    Aggiunto codice per Collapsing con Immagine

--- app/src/main/java/app/com/demonuovaapp/MainActivity.java
+++ app/src/main/java/app/com/demonuovaapp/MainActivity.java
@@ -1,13 +1,15 @@
 package app.com.demonuovaapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbar =
+                (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbar.setTitle("My Toolbar Tittle");
+
+        collapsingToolbar.setCollapsedTitleTextColor(Color.YELLOW);
+        collapsingToolbar.setExpandedTitleColor(Color.BLACK);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
